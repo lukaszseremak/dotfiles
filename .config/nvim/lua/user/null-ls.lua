1466b94c97eb9c697d4cd10ead51af113b081bb6
lukaszseremak <lukasz.dominik.seremak@gmail.com>
2022-02-10 14:16:36 +0100

    feat: add djhtml and shfmt formatting to null_ls

--- .config/nvim/lua/user/null-ls.lua
+++ .config/nvim/lua/user/null-ls.lua
@@ -9,10 +9,12 @@ local diagnostics = null_ls.builtins.diagnostics
 null_ls.setup({
 	debug = false,
 	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
 		formatting.black,
+		formatting.djhtml,
 		formatting.isort,
 		formatting.json_tool,
+		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+		formatting.shfmt,
 		formatting.stylua,
 		formatting.trim_newlines,
 		formatting.trim_whitespace,
