5b9fcd3245c7d66a8a023f28b0baf22a79e6707f
lukaszseremak <lukasz.dominik.seremak@gmail.com>
2022-09-19 15:17:31 +0200

    feat: add ability to display polybar on multiple monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,6 +4,9 @@
 killall -q polybar
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar -c $HOME/.config/polybar/config.ini top 2>&1 | tee -a /tmp/polybar.log & disown
+for monitor in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+	echo "Launching Polybar on monitor: "$monitor
+	MONITOR=$monitor polybar --reload -c $(dirname "$0")/config.ini top 2>&1 | tee -a /tmp/polybar.log &
+done
 
 echo "Polybar launched..."
