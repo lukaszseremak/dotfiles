e0b8c610bd591ef5978bb0861facb860ef0d8e95
lukaszseremak <lukasz.dominik.seremak@gmail.com>
2022-09-24 20:57:43 +0200

    fix: wrong settings for only_laptop.sh script

--- .config/i3/only_laptop.sh
+++ .config/i3/only_laptop.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off
 nitrogen --restore --set-scaled
 i3-msg restart
